2b03d5a42b403a671095291005c1ede2dd1ff57f
nguyenducdo94 <nguyenducdo94@gmail.com>
2023-10-16 14:43:30 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace FacebookTool_GetTokenLD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.rtxtLogs = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(13, 192);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(481, 38);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // rtxtLogs
+            // 
+            this.rtxtLogs.Location = new System.Drawing.Point(13, 32);
+            this.rtxtLogs.Name = "rtxtLogs";
+            this.rtxtLogs.Size = new System.Drawing.Size(481, 154);
+            this.rtxtLogs.TabIndex = 1;
+            this.rtxtLogs.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 237);
+            this.Controls.Add(this.rtxtLogs);
+            this.Controls.Add(this.btnGet);
+            this.Name = "Form1";
+            this.Text = "Công cụ lấy token LD Facebook";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.RichTextBox rtxtLogs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookTool_GetTokenLD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            Task.Run(()=> {
+
+                string adbCommand = "adb";
+                string adbArguments = "shell cat /data/data/com.facebook.katana/app_light_prefs/com.facebook.katana/authentication";
+
+                Process process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = adbCommand,
+                        Arguments = adbArguments,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                string outputData = process.StandardOutput.ReadToEnd();
+
+                string pattern = "access_token\\0(.*?)\\u0005";
+
+                Match match = Regex.Match(outputData, pattern);
+
+                if (match.Success)
+                {
+                    string accessToken = match.Groups[1].Value;
+
+                    WriteLog(accessToken.Substring(1), "SUCCESS");
+                }
+
+            });
+        }
+
+        private void WriteLog(string message, string loglevel)
+        {
+            string formattedMessgae = $"{loglevel}: {message}";
+
+            if (rtxtLogs.InvokeRequired)
+            {
+                rtxtLogs.Invoke(new Action<string, string>(WriteLog), message, loglevel);
+            }
+            else
+            {
+                Color textColor = Color.Black;
+
+                if (loglevel == "ERROR")
+                {
+                    textColor = Color.Red;
+                }
+                else if(loglevel == "SUCCESS")
+                {
+                    textColor = Color.Green;
+                }
+                else if(loglevel == "WARNING")
+                {
+                    textColor = Color.Orange;
+                }
+
+                rtxtLogs.SelectionStart = rtxtLogs.TextLength;
+                rtxtLogs.SelectionLength = 0;
+                rtxtLogs.SelectionColor = textColor;
+                rtxtLogs.AppendText(formattedMessgae + Environment.NewLine);
+                rtxtLogs.SelectionColor = rtxtLogs.ForeColor;
+
+                rtxtLogs.SelectionStart = rtxtLogs.Text.Length;
+                rtxtLogs.ScrollToCaret();
+            }
+        }
+    }
+}
